6e0e512fdec408e0e705de17f8f9346353dd3799
hpozza <henriquepza@gmail.com>
2022-10-13 18:02:30 -0300

    First commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'movies',
+    pathMatch: 'full'
+  },
+  {
+    path: 'movies',
+    loadChildren: () => import('./movie/movie.module').then(r => r.MovieModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
